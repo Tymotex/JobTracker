1e2438970d34a7dd3789b6f072e3a5528c9b3fa5
Tymotex <timzhang3@gmail.com>
2021-03-07 20:21:01 +1100

    Added simple styles and grid on job details page

--- SourceCode_and_Documentation/client/src/AT3K/pages/JobDetails.js
+++ SourceCode_and_Documentation/client/src/AT3K/pages/JobDetails.js
@@ -2,12 +2,38 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import { JobMap } from '../components/job-map';
+import {
+    Grid,
+    Button
+} from '@material-ui/core';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+
+import styles from './JobDetails.module.scss';
 
 const JobDetails = () => {
     return (
         <Layout>
-            <h1>Job Details</h1>
-            [Need a back button. Maybe use a Material UI breadcrumb?]
+            <Grid container>
+                <Grid item xs={9}>
+                    <h1 className={styles.mainTitle}>
+                        Job Details
+                    </h1>
+                    [Need a back button. Maybe use a Material UI breadcrumb?]
+                    <div>
+                        <Button variant="outlined" color="primary">
+                            View official post
+                        </Button>
+                    </div>
+                </Grid>
+                <Grid item xs={3}>
+                    <CalendarTodayIcon /> Posted on: ...
+                    <div>
+                        <Button variant="outlined" color="primary">
+                            View official post
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
 
             <hr />
             <h3>Description</h3>
