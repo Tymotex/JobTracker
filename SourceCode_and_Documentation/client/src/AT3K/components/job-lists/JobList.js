906d7f43b56251ac328b254dec59337a2fb869fa
Arthur <ArthurWang2000@hotmail.com>
2021-03-07 23:43:04 +1100

    Added search bar to job search, replaced jobPost component with material ui card component

--- SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobList.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobList.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import JobPost from './JobPost';
+import { Button, Grid } from "@material-ui/core";
+import React from "react";
+import JobPost from "./JobPost";
+import Searchbar from "./searchbar";
 
 const JobList = ({ data }) => {
-    return (
-        <div>
-            {data.map((eachJobPost) => (
-                <JobPost {...eachJobPost} />
-            )) }
-        </div>
-    )
-}
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={2}>
+          <Searchbar placeholder="Job Search"></Searchbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Searchbar placeholder="Location"></Searchbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Button>Search</Button>
+        </Grid>
+      </Grid>
+      <br></br>
+      <Grid container>
+        {data.map((eachJobPost) => (
+          <Grid item xs={3}>
+          <JobPost {...eachJobPost} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
 
 export default JobList;
--- SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import {
-    Link
-} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 const JobPost = ({ position, company, link }) => {
-    return (
-        <div>
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Position: {position}
+          </Typography>
+          <Typography variant="body2" component="p">
+            Company: {company}
+            <br />
             <div>
-                Position: {position} 
+              <a href={link}>Link to details</a>
             </div>
             <div>
-                Company: {company}
+              <Link to={`/search/details?id=${"JOB_POST_ID_HERE"}`}>
+                See more details
+              </Link>
             </div>
-            <div>
-                <a href={link}>Link to details</a>
-            </div>
-            <div>
-                <Link to={`/search/details?id=${"JOB_POST_ID_HERE"}`}>See more details</Link>
-            </div>
-            <hr />
-        </div>
-    )
-}
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 
 export default JobPost;
