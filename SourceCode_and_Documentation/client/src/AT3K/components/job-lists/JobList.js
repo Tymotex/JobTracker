7086d533323e1ea41174faa68af2eacc40ac6891
Arthur <ArthurWang2000@hotmail.com>
2021-03-08 00:15:32 +1100

    Added toggle for details of job posts, currently only resizes card, doesn't add extra details

--- SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobList.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobList.js
@@ -1,18 +1,45 @@
 import { Button, Grid } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import JobPost from "./JobPost";
 import Searchbar from "./searchbar";
-
+import BoardDropdown from "./detailsToggle";
 const JobList = ({ data }) => {
+  const [boardType, setBoardType] = useState("less".toLowerCase());
+
+  const handleChangeBoard = (event) => {
+    setBoardType(event.target.value);
+  };
+
   return (
     <>
       <Grid container>
         <Grid item xs={2}>
           <Searchbar placeholder="Job Search"></Searchbar>
         </Grid>
+
         <Grid item xs={2}>
-          <Searchbar placeholder="Location"></Searchbar>
+          <BoardDropdown
+            label={"Sort by"}
+            boardType={boardType}
+            handleChangeBoard={null}
+            menuItems={[
+              { value: "less", text: "Less Details" },
+              { value: "more", text: "More Details" },
+            ]}
+          />
         </Grid>
+        <Grid item xs={2}>
+          <BoardDropdown
+            label={"toggle detail"}
+            boardType={boardType}
+            handleChangeBoard={handleChangeBoard}
+            menuItems={[
+              { value: "less", text: "Less Details" },
+              { value: "more", text: "More Details" },
+            ]}
+          />
+        </Grid>
+
         <Grid item xs={2}>
           <Button>Search</Button>
         </Grid>
@@ -20,8 +47,8 @@ const JobList = ({ data }) => {
       <br></br>
       <Grid container>
         {data.map((eachJobPost) => (
-          <Grid item xs={3}>
-          <JobPost {...eachJobPost} />
+          <Grid item xs={boardType == "less" ? 4 : 12}>
+            <JobPost {...eachJobPost} />
           </Grid>
         ))}
       </Grid>
--- SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
@@ -1,13 +1,11 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
+import styles from "./jobPost.module.scss"
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
@@ -30,7 +28,7 @@ const JobPost = ({ position, company, link }) => {
 
   return (
     <>
-      <Card className={classes.root}>
+      <Card className={`${classes.root} ${styles.card}`}>
         <CardContent>
           <Typography variant="h5" component="h2">
             Position: {position}
