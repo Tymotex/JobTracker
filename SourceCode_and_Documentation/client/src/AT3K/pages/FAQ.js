b7130039544006a7515f0ded8eb8d1c0864254c8
Kai <kaisoman56@gmail.com>
2021-03-09 16:19:04 +1100

    Made basic faq page, added accordion thing and title/description

--- SourceCode_and_Documentation/client/src/AT3K/pages/FAQ.js
+++ SourceCode_and_Documentation/client/src/AT3K/pages/FAQ.js
@@ -4,26 +4,29 @@ import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
-
+import Accordion from './accordion'
 import Layout from '../../components/Layout/Layout';
+import styles from './FAQ.module.scss'
 
 const FAQ = () => {
     return (
         <Layout>
-            <h1>FAQ</h1>
-            <p>
-                Hello
+            <h1 className={styles.Title}>FAQ</h1>
+            <p className={styles.text}>
+                Search for your question or browse our frequently asked questions
             </p>
             <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-amount">Search for a question</InputLabel>
+                <InputLabel htmlFor="outlined-adornment-amount">How do i get rid of this!?</InputLabel>
                 <OutlinedInput
                     id="outlined-adornment-amount"
-                    value={"Test"}
+                    placeholder={"Search"}
                     onChange={function() {}}
                     startAdornment={<InputAdornment position="start"></InputAdornment>}
                     labelWidth={60}
                 />
             </FormControl>
+
+            <Accordion/>
         </Layout>
     );
 };
