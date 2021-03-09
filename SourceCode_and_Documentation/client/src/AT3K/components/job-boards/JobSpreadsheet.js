0f3506a5e6798820bba7b440382b32a1e744e512
Tymotex <timzhang3@gmail.com>
2021-03-09 20:58:33 +1100

    Added container styles to settings panel

--- SourceCode_and_Documentation/client/src/AT3K/components/job-boards/JobSpreadsheet.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-boards/JobSpreadsheet.js
@@ -4,12 +4,23 @@ import DataGrid from 'react-data-grid';
 const columns = [
     { key: 'jobPosition', name: 'Job Position' },
     { key: 'companyName', name: 'Company Name' },
-    { key: "salary", name: "salary" }
+    { key: "salary", name: "salary" },
+    { key: "etc", name: "etc..." }
 ];
 
 const rows = [
     { jobPosition: "Frontend Dev", companyName: "Canva", salary: "$50000/yr" },
-    { jobPosition: "Security Engineer", companyName: "Citadel", salary: "$120000/yr" }
+    { jobPosition: "Security Engineer", companyName: "Citadel", salary: "$120000/yr" },
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"},
+    { jobPosition: "NEWLY TRACKED JOB POSTINGS ARE AUTOMATICALLY", companyName: "GENERATED", salary: "HERE"}
 ];
 
 
--- SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
+++ SourceCode_and_Documentation/client/src/AT3K/components/job-lists/JobPost.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { makeStyles } from "@material-ui/core/styles";
@@ -43,6 +44,11 @@ const JobPost = ({ position, company, link }) => {
               <Link to={`/search/details?id=${"JOB_POST_ID_HERE"}`}>
                 See more details
               </Link>
+            </div>
+            <div>
+              <Button variant="contained" color="info">
+                Track
+              </Button>
             </div>
           </Typography>
         </CardContent>
