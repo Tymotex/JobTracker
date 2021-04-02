49b02a194c2a463f2188ce608934f84975ec235e
Tymotex <timzhang3@gmail.com>
2021-04-02 18:03:44 +1100

    Updated npm dependencies

--- SourceCode_and_Documentation/server/JobTracker/api_routes/auth.py
+++ SourceCode_and_Documentation/server/JobTracker/api_routes/auth.py
@@ -11,6 +11,9 @@ from JobTracker.database_ops import (
     add_user,
     login_user
 )
+from JobTracker.exceptions import (
+    InvalidUserInput
+)
 from JobTracker.utils.colourisation import printColoured
 from flask_restx import Resource, Api, fields
 
@@ -47,14 +50,18 @@ class AuthenticationRegister(Resource):
     def post(self):
         printColoured(" * Registering a new user", colour="yellow")
         request_params = dict(request.form)
-        username = request_params["username"]
-        email = request_params["email"]
-        password = request_params["password"]
-        user_id = add_user(username, email, password)
-        return {
-            "user_id": user_id,
-            "token": "EMPTY"
-        }   
+        try:
+            username = request_params["username"]
+            email = request_params["email"]
+            password = request_params["password"]
+            user_id = add_user(username, email, password)
+            return {
+                "user_id": user_id,
+                "token": "EMPTY"
+            }   
+        except Exception as err:
+            printColoured(err, colour="red")
+            raise err
 
 @auth_api.route('/login')
 class AuthenticationLogin(Resource):
