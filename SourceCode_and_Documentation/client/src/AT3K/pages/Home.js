eaf08b490bd292d689983c7c18350941bed72654
Tim Zhang <timzhang3@gmail.com>
2021-05-30 10:28:50 +0000

    Removed forced redirect on home after google authentication

--- SourceCode_and_Documentation/client/src/AT3K/pages/Home.js
+++ SourceCode_and_Documentation/client/src/AT3K/pages/Home.js
@@ -3,17 +3,15 @@ import React from 'react';
 import FadeIn from 'react-fade-in';
 import { useParams, withRouter } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout';
-import Newsfeed from './Newsfeed';
+// import Newsfeed from './Newsfeed';
 import WelcomePage from './WelcomePage';
 
 const Home = withRouter(({ unknownPath, history }) => {
     const { id: userID, token } = useParams();
-    const isLoggedIn = userID;
+    // const isLoggedIn = userID;
     if (userID) {
         Cookie.set('user_id', userID);
         Cookie.set('token', token);
-        history.push('/');
-        window.location.reload();
     }
 
     return (
